bf4d604a6ea6d6da7d6db71d2bcad6720642a1d0
Rim Ouarrak <rouarrak@e1r13p2.1337.ma>
2023-06-04 09:40:20 +0100

    modif exit stat

--- builtin/exit.c
+++ builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: rouarrak <rouarrak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 05:21:33 by rouarrak          #+#    #+#             */
-/*   Updated: 2023/06/04 08:12:01 by rouarrak         ###   ########.fr       */
+/*   Updated: 2023/06/04 09:38:07 by rouarrak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,12 @@ int     is_int(char *arg)
     
     len = ft_strlen(arg);
     i = 0;
+    
+    if (arg[0] == '-' && arg[1])
+        i++;
     while (i < len)
     {
-        if (arg[i] <= '0' || arg[i] >= '9')
+        if (arg[i] < '0' || arg[i] > '9')
             return (0);
         i++;
     }
@@ -31,14 +34,20 @@ int     is_int(char *arg)
 void    bsh_exit(t_cmd *cmd)
 {
     if (!cmd->args[1])
+    {
+        printf("exit\n");
         exit (0);
+    }
+    if (is_int(cmd->args[1]))
+    {
+        printf("exit\n");
+        exit (ft_atoi(cmd->args[1]));
+    }
     else if (cmd->args[2])
         printf("bash: exit: too many arguments\n");
-    else if (!is_int(cmd->args[1]))
+    else
     {
         printf("exit\nbash: exit: %s: numeric argument required\n", cmd->args[1]);
         exit (255);
     }
-    else
-        exit (ft_atoi(cmd->args[1]));
 }
--- exec/cmd.c
+++ exec/cmd.c
@@ -6,7 +6,7 @@
 /*   By: rouarrak <rouarrak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 23:42:59 by rouarrak          #+#    #+#             */
-/*   Updated: 2023/06/04 08:08:16 by rouarrak         ###   ########.fr       */
+/*   Updated: 2023/06/04 08:19:40 by rouarrak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,6 @@ void    exec(t_linkedlist *lexer, t_ast ast)
 	arg = ast.root->content->cmd;
 	if (!ft_strncmp("exit", cmd, 5))
 		bsh_exit(arg);
+	if (!ft_strncmp("echo", cmd, 5))
+		bsh_echo(lexer->head);
 }
