3df438f23358e74be2e43282ecc8c952479bdd09
Rim Ouarrak <rouarrak@e1r13p2.1337.ma>
2023-06-04 08:13:44 +0100

    -sgf exit

--- builtin/exit.c
+++ builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: rouarrak <rouarrak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 05:21:33 by rouarrak          #+#    #+#             */
-/*   Updated: 2023/06/04 06:36:18 by rouarrak         ###   ########.fr       */
+/*   Updated: 2023/06/04 08:12:01 by rouarrak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,15 @@ int     is_int(char *arg)
 
 void    bsh_exit(t_cmd *cmd)
 {
-    if (!is_int(cmd->args[1]))
+    if (!cmd->args[1])
+        exit (0);
+    else if (cmd->args[2])
+        printf("bash: exit: too many arguments\n");
+    else if (!is_int(cmd->args[1]))
     {
         printf("exit\nbash: exit: %s: numeric argument required\n", cmd->args[1]);
         exit (255);
     }
-    else if (cmd->args[2])
-        printf("bash: exit: too many arguments\n");
-    else if (!cmd->args[1])
-        exit (0);
     else
         exit (ft_atoi(cmd->args[1]));
 }
