ae1c6537fc9bf3f3f6a4a95c3db2bc67e3c1c690
Amine Beihaqi <abeihaqi@e2r12p3.1337.ma>
2023-06-13 05:54:29 +0100

    Wildcard bug

--- lexer/is_token.c
+++ lexer/is_token.c
@@ -6,7 +6,7 @@
 /*   By: abeihaqi <abeihaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 07:07:16 by abeihaqi          #+#    #+#             */
-/*   Updated: 2023/06/10 04:07:20 by abeihaqi         ###   ########.fr       */
+/*   Updated: 2023/06/12 02:10:19 by abeihaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ int	is_token(char c)
 	return (ft_isspace(c) || c == NEW_LINE || c == QUOTE
 		|| c == DOUBLE_QUOTE || c == ENV || c == '&'
 		|| c == PIPE_LINE || c == REDIRECTION_IN
-		|| c == REDIRECTION_OUT || c == WILDCARD
+		|| c == REDIRECTION_OUT
 		|| c == PARENTASIS_OPEN || c == PARENTASIS_CLOSE);
 }
