1846327c5432c89859dad469bd75eb6f0d7e470d
Amine Beihaqi <abeihaqi@e1r1p3.1337.ma>
2023-07-02 23:20:41 +0100

    echo

--- utils/get_env.c
+++ utils/get_env.c
@@ -6,7 +6,7 @@
 /*   By: abeihaqi <abeihaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 07:14:35 by abeihaqi          #+#    #+#             */
-/*   Updated: 2023/06/26 00:03:07 by abeihaqi         ###   ########.fr       */
+/*   Updated: 2023/07/02 23:18:13 by abeihaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ char	*get_env_variable(char *name)
 		return (ft_itoa(g_data.exit_status));
 	while (env)
 	{
-		if (!ft_strncmp(name, env->key, ft_strlen_alnum(env->key))
+		if (!ft_strncmp(name, env->key, ft_strlen_alnum(name))
 			&& env->value)
 			return (ft_strdup(env->value));
 		env = env->next;
